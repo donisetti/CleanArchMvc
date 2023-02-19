17348a135345e2dd4c53a5b8d0cd7a52b422749e
Donisetti Ferreira Cosma <donisetti@msn.com>
2023-02-19 10:21:35 -0400

    POC

---
+++ Vasis.Gestor.Application/Interfaces/IEmpresaService.cs
@@ -0,0 +1,14 @@
+﻿using Vasis.Gestor.Application.DTOs;
+
+namespace Vasis.Gestor.Application.Interfaces;
+
+public interface IEmpresaService
+{
+
+    Task<IEnumerable<EmpresaDTO>> ObterTodos();
+    Task<EmpresaDTO> ObterPorId(int? id);
+    Task Incluir(EmpresaDTO categoryDto);
+    Task Alterar(EmpresaDTO categoryDto);
+    Task Excluir(int? id);
+
+}
---
+++ Vasis.Gestor.Application/Interfaces/IVendedorService.cs
@@ -0,0 +1,13 @@
+﻿using Vasis.Gestor.Application.DTOs;
+
+namespace Vasis.Gestor.Application.Interfaces;
+
+public interface IVendedorService
+{
+
+    Task<IEnumerable<VendedorDTO>> ObterTodos();
+    Task<VendedorDTO> ObterPorId(int? id);
+    Task Incluir(VendedorDTO categoryDto);
+    Task Alterar(VendedorDTO categoryDto);
+    Task Excluir(int? id);
+}
